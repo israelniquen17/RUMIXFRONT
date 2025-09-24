d25dc1e1419e3cb5b0e19060770bca11539eb919
OrlanDev <Oniquenneciosup@gmail.com>
2025-09-24 01:54:26 -0500

    Primer commit - proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio';
+import { ProductosComponent } from './components/productos/productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', component: InicioComponent },
+     { path: 'productos', component: ProductosComponent },
+    { path: '**', redirectTo: '' }            
+];
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter, RouterModule } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
+bootstrapApplication(App, {
+  providers: [
+ importProvidersFrom(
+      HttpClientModule,
+      RouterModule.forRoot(routes) 
+    )
+  ]
+});
